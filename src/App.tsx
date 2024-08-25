857eb361a0b4c48cf43c130d8dd92214e4e0afd3
Stefano Scalfari <vorshim92@gmail.com>
2024-08-25 02:37:15 +0200

    update light and sfocatura

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,19 @@ export default function App() {
     >
       <ambientLight intensity={0.5} />
 
-      {/* Luce direzionale con ombre */}
-      <directionalLight position={[5, 10, 5]} intensity={1} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
+      {/* Luce direzionale (simulazione del sole) */}
+      <directionalLight
+        castShadow
+        position={[5, 10, 5]}
+        intensity={1.5}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
 
       {/* Punto luce principale */}
       <pointLight position={[10, 10, 10]} intensity={0.8} castShadow />
@@ -20,15 +31,15 @@ export default function App() {
       <RotatingMap />
 
       {/* Sfocatura */}
-      {/* <mesh>
+      <mesh position={[0, 0, 3]}>
         <planeGeometry args={[100, 100]} />
         <meshBasicMaterial transparent opacity={0.3} color="black" />
-      </mesh> */}
+      </mesh>
 
       {/* Logo del gioco */}
       <Html position={[0, 1.5, 0]} center>
         <div style={{ textAlign: "center", color: "white", fontSize: "2rem" }}>
-          <img src="/logo.png" alt="Game Logo" style={{ width: "800px" }} />
+          <img src="/logo.png" alt="Game Logo" style={{ width: "800px" }} draggable="false" />
         </div>
       </Html>
 
@@ -46,12 +57,12 @@ export default function App() {
             alignItems: "center",
           }}
         >
-          <img src="/sword-icon.png" alt="Start" style={{ height: "140px", marginRight: "20px" }} />
+          <img src="/sword-icon.png" alt="Start" style={{ height: "140px", marginRight: "20px" }} draggable="false" />
           START
         </button>
       </Html>
 
-      <OrbitControls enableZoom={false} enableRotate={false} />
+      <OrbitControls enableZoom={false} enableRotate={false} enablePan={false} />
     </Canvas>
   );
 }
